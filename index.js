4855be16a9e8cd14a430e4a5bd93643f548d1840
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-03-22 05:37:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+var superbowlWin = superbowlWinRecord => {
+	let superbowlWon = superbowlWinRecord.find( superbowl => {  return superbowl.result === "W" })
+	if (superbowlWon){ 
+		return superbowlWon.year
+	} else {
+		return undefined
+	}
+}
